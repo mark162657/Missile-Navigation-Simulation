6036663eb2aaf02936541975f6f878dfb2cd4f4f
markscp162657 <markscp162657@gmail.com>
2026-06-26 14:27:52 +0800

    Flight control setup

--- src/missile/control/autopilot.py
+++ src/missile/control/autopilot.py
@@ -2,12 +2,19 @@ import numpy as np
 import numpy.typing as npt
 
 from missile.profile import MissileProfile
+from missile.state import MissileState
 
 class AutoPilot:
-    def __init__(self, trajectory: npt.NDArray[np.float64], profile: MissileProfile):
+    def __init__(self, trajectory: npt.NDArray[np.float64], profile: MissileProfile, state: MissileState):
         """
         Args:
             trajectory: a non-writable numpy array of shape (N, 3), with lat/lon/terrain_elevation
         """
         self.trajectory = trajectory
-        self.profile = profile
+        self.profile = MissileProfile()
+        self.state = MissileState()
+
+
+
+
+
