1cfc8fbc0ad04f440901680bbfb6650ccc9c1cd1
markscp162657 <markscp162657@gmail.com>
2026-06-28 14:26:41 +0800

    Building foundations of pid controller

--- src/missile/control/pid_controller.py
+++ src/missile/control/pid_controller.py
@@ -1,11 +1,37 @@
 class PIDController:
-    def __init__(self, P: float, I: float, D: float, output_max: float, output_min: float):
-        self.Kp = P
-        self.Ki = I
-        self.Kd = D
+    def __init__(self, P: float, I: float, D: float, out_max: float, out_min: float):
+        """"""
+        if out_max > out_min:
+            raise ValueError(f"out_min: {out_min} must be less than out_max: {out_max}.")
+        self.Kp = float(P)
+        self.Ki = float(I)
+        self.Kd = float(D)
 
-        self.output_max = output_max
-        self.output_min = output_min
+        self.out_max = float(out_max)
+        self.out_min = float(out_min)
+
+        self.integral = 0.0 # I: integral controllers accumulate error over time (error * dt), unlike D and P
+        self.prev_mea = None
 
 
+    def update(self, error: float, measurement: float, dt: float) -> float:
+        if dt <= 0.0: # when dt is broken (cannot be ≤ 0), we will only rely on P controller
+            raise ValueError("dt must be greater than 0.0")
+
+        # P
+        # I
+        # D
+        
+
+
+
+
+
+
+    def reset(self) -> None:
+        """
+        Reset integral and previous measurements when switching from: boost -> cruise phase
+        """
+        self.integral = 0.0
+        self.prev_mea = None
 
