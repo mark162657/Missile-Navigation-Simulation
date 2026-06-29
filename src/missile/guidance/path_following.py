153fa19bebc978e8fa8b4f6db53661de6e4bd228
markscp162657 <markscp162657@gmail.com>
2026-06-29 16:06:18 +0800

    Constructing pathfollower

--- src/missile/guidance/path_following.py
+++ src/missile/guidance/path_following.py
@@ -1,2 +1,18 @@
+import numpy as np
+import math
+
+from missile.planning.trajectory import TrajectoryGenerator
+from missile.profile import MissileProfile
+from terrain.coordinates import CoordinateSystem
+
 class PathFollower:
-    pass
+    def __init__(
+            self,
+            trajectory: TrajectoryGenerator,
+            profile: MissileProfile,
+            coordinate: CoordinateSystem
+    ):
+        self.trajectory = trajectory
+        self.profile = profile
+
+        
