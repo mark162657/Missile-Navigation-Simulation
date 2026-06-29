15f40b6457840b0bfffd80a5bbe0407ceca1f0e8
markscp162657 <markscp162657@gmail.com>
2026-06-29 15:44:51 +0800

    Finished updating altitude and speed/throttle command in AutoPilot using PIDController

--- src/missile/controls/autopilot.py
+++ src/missile/controls/autopilot.py
@@ -1,3 +1,8 @@
+"""
+Autopilot serves as an outer layer that calls the PID controller at the bottom of the simulation.
+It handles mainly altitude and speed, while turn is the job for guidance.
+"""
+
 import numpy as np
 import numpy.typing as npt
 
@@ -30,7 +35,9 @@ class AutoPilot:
     ) -> ControlInput:
         """
         Receive input of what guidance want + where we are now + how much time passed.
-        Pass them into PID and we make the output into ControlInput
+        Pass them into PID and we make the output into ControlInput.
+        As turn acceleration was handled in path_following / guidance related components,
+        we will handle speed: throttle, and climb here.
 
         Args:
             state: the MissileState
@@ -46,6 +53,25 @@ class AutoPilot:
         # handles error for PID (target - measurement)
         alt_error = target_alt - curr_alt
         spd_error = target_spd - curr_spd
+
+        # altitude command
+        accel_climb = _G + self.alt_pid.update(alt_error, curr_alt, dt)
+
+        # speed command
+        throttle = self.spd_pid.update(spd_error, curr_spd, dt)
+
+        accel_turn = lateral_accel_cmd
+
+        return ControlInput(
+            throttle=throttle,
+            accel_turn = accel_turn,
+            accel_climb = accel_climb
+        )
+
+    def reset(self):
+        """Clearing both PID when transition of target, and boost -> cruise phase"""
+        self.alt_pid.reset()
+        self.spd_pid.reset()
 
 
 
