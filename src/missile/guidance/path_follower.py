2b6b337c59b6ad46e1ae1c03882bea44f34596ef
markscp162657 <markscp162657@gmail.com>
2026-06-30 17:56:48 +0800

    Turned path into lat/lon -> enu, and extracted elevatio into a numpy array, and added a function to get target altitude missile trying to maintain

--- src/missile/guidance/path_follower.py
+++ src/missile/guidance/path_follower.py
@@ -14,8 +14,34 @@ class PathFollower:
             coordinate: CoordinateSystem,
             l1_distance: float=300.0
     ):
+
+        # dealing with path
         self.path = trajectory.get_trajectory()
+        self.path_length = len(self.path)
+
+        # set up the profile and coordinate system
         self.profile = profile
+        self.coord = coordinate
+
+
+        # convert path coordinates to enu
+        if self.path.ndim != 2 or self.path.shape[1] < 2:
+            raise ValueError(f"Expected trajectory shape (N, >=2), got {self.path.shape}")
+
+        lat_lon = self.path[:, :2] # all rows, column from idx 0 up to 2
+        self.traj_enu = np.asarray(
+            [self.coord.latlong_to_enu(float(lat), float(lon)) for lat, lon in lat_lon], dtype=float
+        )
+        self.traj_enu.setflags(write=False)
+
+        # extract ground elevation from path
+        self.ground_elev = self.path[:, 2]
+
+        self.l1 = l1_distance
+        self.last_idx = 0
+
+    def update(self):
+        pass
 
     def _l1_lateral_accel(
             self,
@@ -25,6 +51,19 @@ class PathFollower:
             aim_pt
     ) -> float:
         pass
+
+    def _target_altitude(self, aim_idx: int) -> float:
+        """
+        Return target altitude, which is the ground elevation + preferred altitude AGL
+
+        Args:
+            aim_idx: the current index of path data
+
+        Return:
+            The target altitude.
+        """
+        pref_alt = self.profile.preferred_agl()
+        return self.ground_elev[aim_idx] + pref_alt
 
 
 
