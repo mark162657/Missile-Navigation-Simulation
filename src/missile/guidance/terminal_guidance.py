b72c1180741ac6a5dd530ae7c50dd523a251620a
markscp162657 <markscp162657@gmail.com>
2026-07-04 16:12:56 +0800

    done flight path angle calculation for terminal guidance

--- src/missile/guidance/terminal_guidance.py
+++ src/missile/guidance/terminal_guidance.py
@@ -23,3 +23,27 @@ class TerminalGuidance:
 
     def terminal_init_range(self):
         pass
+
+    def _flight_path_angle(self, state: MissileState):
+        """
+        Get the flight path angle (relative to horizontal) of the missile.
+        Mathematical expression:
+            γm = atan2(v_up, sqrt(v_east^2 + v_north^2))
+
+        Args:
+            state: current missile state
+        """
+        return np.artan2(state.vel_up, np.sqrt(state.vel_east**2 + state.vel_north**2))
+
+    def _radius_of_curvature(self):
+        """
+        Calculate the radius of curvature at the “nearest” point on ellipse
+
+        Mathematical expression:
+            R_C(t) = (a^2 * sin^2 t + b^2 * cos^2 t)^(3/2) / (a * b)
+
+        Reference:
+            Ellipse radius of curvature. (n.d.).
+            SKM Classes Bangalore. https://skmclasses.weebly.com/ellipse-radius-of-curvature.html
+        """
+        pass
