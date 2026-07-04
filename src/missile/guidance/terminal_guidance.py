116df47375c51ea56104f21974a33c1fef790696
markscp162657 <markscp162657@gmail.com>
2026-07-04 15:45:46 +0800

    Terminal guidance init stuff

--- src/missile/guidance/terminal_guidance.py
+++ src/missile/guidance/terminal_guidance.py
@@ -1,11 +1,25 @@
 import math
 import numpy as np
 
+from guidance.target_geometry import TargetGeometry
 from missile.state import MissileState
+from profile import MissileProfile
+
+_G = 9.80665
 
 class TerminalGuidance:
-    def __init__(self):
+    def __init__(
+            self,
+            target: TargetGeometry,
+            profile: MissileProfile,
+            impact_angle_deg: float,
+            approach_azimuth_rad: float,
+
+    ):
         pass
 
-    def step(self, state: MissileState, dt: float):
+    def update(self, state: MissileState, dt: float):
+        pass
+
+    def terminal_init_range(self):
         pass
