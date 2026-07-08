c81dca6e7e07113871d53bd8913745905d518125
markscp162657 <markscp162657@gmail.com>
2026-07-08 13:41:38 +0800

    Fixed some bug in FlightComputer

--- src/missile/controls/flight_computer.py
+++ src/missile/controls/flight_computer.py
@@ -1,24 +1,27 @@
-from controls.control_input import ControlInput
-from guidance.target_geometry import TargetGeometry
+import numpy.typing as npt
+
+from missile.controls.control_input import ControlInput
+from missile.guidance.target_geometry import TargetGeometry
 from missile.controls.autopilot import AutoPilot
 from missile.state import MissileState, FlightStage
 from missile.profile import MissileProfile
-from missile.planning.trajectory import TrajectoryGenerator
-from guidance.path_follower import PathFollower
-from guidance.terminal_guidance import TerminalGuidance
+from missile.guidance.path_follower import PathFollower
+from missile.guidance.terminal_guidance import TerminalGuidance
+from terrain.coordinates import CoordinateSystem
 
 
 class FlightComputer:
     def __init__(
             self,
-            trajectory: TrajectoryGenerator,
+            trajectory: npt.NDArray,
             profile: MissileProfile,
             target: TargetGeometry,
+            coordinate: CoordinateSystem,
             impact_angle_deg: float,
             approach_azimuth_rad: float,
             lookahead_dist: float=300.0
     ):
-        self.path_follower = PathFollower(trajectory, profile, lookahead_dist=lookahead_dist)
+        self.path_follower = PathFollower(trajectory, profile, coordinate, lookahead_dist=lookahead_dist)
         self.terminal = TerminalGuidance(target, profile, impact_angle_deg, approach_azimuth_rad)
         self.autopilot = AutoPilot(profile)
         self._terminal_latched = False
@@ -44,7 +47,6 @@ class FlightComputer:
     def _step_cruise(self, state: MissileState, dt: float) -> ControlInput:
         """Updating the path-following guidance and then returning the control input"""
         lateral_accel, target_alt, target_spd = self.path_follower.update(state)
-        self.autopilot.update(state, target_alt, target_spd, lateral_accel, dt)
         return self.autopilot.update(state, target_alt, target_spd, lateral_accel, dt)
 
     def _step_terminal(self, state: MissileState, dt: float) -> ControlInput:
@@ -55,8 +57,8 @@ class FlightComputer:
 
     def _speed_throttle(self, state: MissileState, target_spd: float, dt: float) -> float:
         """Return the throttle value from the PID controller based on current and target speed"""
-        curr_spd = self.state.get_ground_speed()
-        spd_error = self.target_spd - curr_spd
+        curr_spd = state.get_ground_speed()
+        spd_error = target_spd - curr_spd
         return self.autopilot.spd_pid.update(spd_error, curr_spd, dt)
 
     def _reset(self) -> None:
