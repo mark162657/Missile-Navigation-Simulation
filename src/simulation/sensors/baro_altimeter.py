d8588af0e0dd182768d77c27e566a33c24526182
markscp162657 <markscp162657@gmail.com>
2026-07-16 15:37:04 +0800

    Added some factors to kd in pid controller for testing

--- src/simulation/sensors/baro_altimeter.py
+++ src/simulation/sensors/baro_altimeter.py
@@ -2,5 +2,13 @@ import numpy as np
 
 class BaroAltimeter:
     def get_baro_msl(self, true_alt: float) -> float:
-        """Apply error on the true altitude (MSL). 0.5 = +- 50cm (Source: BMP388)"""
+        """
+        Apply error on the true altitude (MSL). 0.5 = +- 50cm (Source: BMP388)
+
+        Args:
+            true_alt: the true altitude of the missile.
+
+        Return:
+            The MSL altitude that are added error to.
+        """
         return float(true_alt) + np.random.normal(0, 0.5)
